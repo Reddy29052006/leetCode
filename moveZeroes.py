7fc948abcd02c92b5bc0309bba0fa77524629645
Reddy <reddysekhar24200706@gmail.com>
2026-06-12 11:25:16 +0530

    Implement moveZeroes function to shift zeros in-place

--- moveZeroes.py
+++ moveZeroes.py
@@ -1,10 +1,25 @@
+from typing import List
+
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         l = 0
         r = 0
         n = len(nums)
         while r < n:
-            pass
+
+            if nums[r] != 0:
+                if r != l:
+                    nums[l] = nums[r]
+                    nums[r] = 0
+                l += 1
+
+            r += 1
+
+        return None
+
+
+solution = Solution()
+nums = [0, 1, 0, 3, 12]
+solution.moveZeroes(nums)
+print(nums)
